709175249efdb5d4f6b19fe7d162b4d4e90b2c86
Juan C. Jimenez Falcetta <140984823+Nheonz@users.noreply.github.com>
2025-03-05 00:00:21 +0100

    refactor(components/todolist.js): se agrego funcion para agregar tareas

--- components/todolist.js
+++ components/todolist.js
@@ -2,3 +2,20 @@ import React, { useState } from "react"; // Contiene todas las tareas
 import TodoItem from "./todoitem";
 
 export default todolist;
+
+const TodoList = () => {
+    const [todos, setTodos] = useState([]); //Contiene todas las tareas
+    const [newTodoText, setNewTodoText] = useState(""); // El texto que un usuario escribe para agregar una nueva tarea
+  
+    // <--- Esta función agrega una nueva tarea al arreglo "todos" y luego limpia el campo de texto --->
+    const handleAddTodo = () => {
+      if (newTodoText.trim()) {
+        const newTodo = {
+          id: Date.now(),
+          text: newTodoText,
+          completed: false,
+        };
+        setTodos([...todos, newTodo]); // Agrega la nueva tarea al estado
+        setNewTodoText(""); // Limpia el campo de entrada
+      }
+    };
