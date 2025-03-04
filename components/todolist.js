057dfa904495fd8f1516487ccf9f462d560c9e28
Juan C. Jimenez Falcetta <140984823+Nheonz@users.noreply.github.com>
2025-03-05 00:08:26 +0100

    refactor(components/todolist.js): se agrega funcionalidad de tarea completada y eliminar tarea por id

--- components/todolist.js
+++ components/todolist.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react"; // Contiene todas las tareas
 import TodoItem from "./todoitem";
 
-export default todolist;
-
 const TodoList = () => {
-    const [todos, setTodos] = useState([]); //Contiene todas las tareas
-    const [newTodoText, setNewTodoText] = useState(""); // El texto que un usuario escribe para agregar una nueva tarea
-  
-    // <--- Esta función agrega una nueva tarea al arreglo "todos" y luego limpia el campo de texto --->
-    const handleAddTodo = () => {
-      if (newTodoText.trim()) {
-        const newTodo = {
-          id: Date.now(),
-          text: newTodoText,
-          completed: false,
-        };
-        setTodos([...todos, newTodo]); // Agrega la nueva tarea al estado
-        setNewTodoText(""); // Limpia el campo de entrada
-      }
-    };
+  const [todos, setTodos] = useState([]); //Contiene todas las tareas
+  const [newTodoText, setNewTodoText] = useState(""); // El texto que un usuario escribe para agregar una nueva tarea
+
+  // <--- Esta función agrega una nueva tarea al arreglo "todos" y luego limpia el campo de texto --->
+  const handleAddTodo = () => {
+    if (newTodoText.trim()) {
+      const newTodo = {
+        id: Date.now(),
+        text: newTodoText,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]); // Agrega la nueva tarea al estado
+      setNewTodoText(""); // Limpia el campo de entrada
+    }
+  };
+
+  const handleToggleComplete = (id) => {
+    setTodos(
+      todos.map(
+        (todo) =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo // Marca tarea como completada
+      )
+    );
+  };
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id)); // Elimina la tarea por id
+  };
+};
+
+export default todolist;
